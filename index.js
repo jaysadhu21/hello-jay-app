a033e27de39138bd29656aa5f16c527985464d02
jay sadhu <94530764+jaysadhu21@users.noreply.github.com>
2025-05-28 18:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
       <title>Hello from Jay sadhu</title>
     </head>
     <body>
-      <h1>Yudiz Solutions</h1>
+      <h1>AWS Pipeline by GT</h1>
     </body>
     </html>
   `);
