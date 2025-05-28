77431af0c57ea6467bfea78813427cb387e5caf9
Ubuntu <ubuntu@ip-172-31-25-26.ap-southeast-1.compute.internal>
2025-05-28 09:43:47 +0000

    Fix permissions in install_pm2.sh to resolve EACCES error

--- scripts/install_pm2.sh
+++ scripts/install_pm2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo npm install -g pm2
 cd /home/ubuntu/hello-jay-app
+# Fix permissions to ensure the ubuntu user can write to the directory and files
+sudo chown -R ubuntu:ubuntu /home/ubuntu/hello-jay-app
+sudo chmod -R u+w /home/ubuntu/hello-jay-app
 npm install
