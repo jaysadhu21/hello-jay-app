e5d116ef7f69f453fade1069afdb9b0292cfffd0
Ubuntu <ubuntu@ip-172-31-25-26.ap-southeast-1.compute.internal>
2025-05-28 11:33:22 +0000

    hiii

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
       <title>Hello from Jay sadhu</title>
     </head>
     <body>
-      <h1>Hello from mavani</h1>
+      <h1>Hii Hello from mavani</h1>
     </body>
     </html>
   `);
