2cfde8723f19839fb16f836d2d096f7314c3f601
jay sadhu <94530764+jaysadhu21@users.noreply.github.com>
2025-06-03 12:30:29 +0530

    Update install_pm2.sh

--- scripts/install_pm2.sh
+++ scripts/install_pm2.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/hello-jay-app
 # Fix permissions to ensure the ubuntu user can write to the directory and files
 sudo chown -R ubuntu:ubuntu /home/ubuntu/hello-jay-app
 sudo chmod -R u+w /home/ubuntu/hello-jay-app
-npm install
+npm insta
